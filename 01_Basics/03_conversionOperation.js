63e1a3c6f805e12ccb5b1ee813165cf5f4458fa5
Akash Chauhan <121815603+akashchauhan555@users.noreply.github.com>
2024-04-24 07:40:11 +0000

    conversion of datatype

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "33asjd"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33ajs" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(typeof isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Akash" => true
+
+
+let someNumber = 44
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
