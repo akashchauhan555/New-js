8dbd21493c502ba60b1423ba321b86b82967336c
Akash Chauhan <121815603+akashchauhan555@users.noreply.github.com>
2024-04-24 16:48:34 +0000

    operation

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -26,6 +26,18 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 44
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
 
+/*************************************Operation*************************************/
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/2);
+console.log(2%2);
