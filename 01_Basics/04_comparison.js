a0fa5fcea99d5e35416321a8934e651b98979e9f
Akash Chauhan <121815603+akashchauhan555@users.noreply.github.com>
2024-04-25 05:08:19 +0000

    completed

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,27 @@
+// console.log(2 > 1);                   //true
+// console.log(2 >= 1);                  //true
+// console.log(2 < 1);                   //false
+// console.log(2 == 1);                  //false
+// console.log(2 != 1);                  //true
+
+
+
+// console.log("2" > 1);                   //true
+// console.log("02" > 1);                  //true 
+
+
+// console.log(null > 0);                     //false
+// console.log(null == 0);                    //false
+// console.log(null >= 0);                    //true
+
+
+// console.log(undefined == 0);                  //false
+// console.log(undefined > 0);                   //false
+// console.log(undefined > 0);                   //false
+
+
+
+console.log("3" === 3);                       //false
+console.log(3 === 3);                         //true
+
+// NOTE:In === equality check it check the datatypes also so (1) => false and (2) => true
