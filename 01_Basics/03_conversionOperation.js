386f0f5dc9919c7451c3c660c9798a3b3ab24732
Akash Chauhan <121815603+akashchauhan555@users.noreply.github.com>
2024-04-25 04:22:30 +0000

    complete

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -35,9 +35,51 @@ let value = 3
 let negValue = -value
 // console.log(negValue);
 
-console.log(2+2);
-console.log(2-2);
-console.log(2*2);
-console.log(2**3);
-console.log(2/2);
-console.log(2%2);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1 = "hello"
+let str2 = " akash"
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);                   //12
+// console.log(1 + "2");                   //12
+// console.log("1" + 2 + 2);               //122
+// console.log(1 + 2 + "2");               //32
+
+// console.log((3 + 4) * 5 % 3);            //2
+
+
+
+// console.log(+true);
+// console.log(+"");
+
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+
+
+
+/***********************************Increment Operator*****************************************/
+
+
+let x = 3
+const y = x++                        //postfix
+// console.log(`x:${x} y:${y}`);
+
+// Output => x:4 y:3
+
+
+let a = 4
+const b = ++a                        //prefix
+console.log(`a:${a} b:${b}`);
+
+// Output => a:5 b:5
+
