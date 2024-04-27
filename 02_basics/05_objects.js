73b468aa92e21464adf620e8e6e951479eb06eac
Akash Chauhan <121815603+akashchauhan555@users.noreply.github.com>
2024-04-27 07:00:24 +0000

    done

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,26 @@
+const course = {
+    coursename:"JavaScript",
+    courseInstructor:"hitesh",
+    price:"999"
+}
+
+// course.courseInstructor
+
+const {courseInstructor:instructor} = course
+
+// console.log(courseInstructor);                    // => hitesh
+console.log(instructor);                            // => hitesh
+
+/*JSON SYNTAX */
+
+// {
+//     "name": "akash",
+//     "coursename": "JavaScript",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
