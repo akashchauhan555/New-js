d8c25be6984c2fd1d8f976fe38813e702eae9eee
Akash Chauhan <121815603+akashchauhan555@users.noreply.github.com>
2024-05-01 11:24:13 +0000

    some topics left

---
+++ 04_control_flow/03_truthy.js
@@ -0,0 +1,38 @@
+// const userEmail = "akash@3949gmail.com"
+const userEmail = ""
+
+// if(userEmail) {
+//   console.log("Got user email");                             // Got user email
+// } else {
+//   console.log("Don't have user email")                      // Don't have user email
+// }
+
+// falsy values
+
+/*false, 0, -0, bigint 0n, "" or '', null, undefined, NaN*/
+
+// truthy values
+
+/* "0", 'false', " ", [], {}, function(){} */
+
+
+// const newUser = []
+
+// if(newUser.length === 0) {
+//   console.log("Array is empty");                   
+// }
+
+// const newObj = {}
+
+// if(Object.keys(newObj).length === 0) {
+//   console.log("Object is empty");                              // Object is empty 
+// }
+
+/*********************************************Investigating study******************************************************/
+
+false == 0                // => true
+false == ''               // => true
+0 == ''                   // => true
+
+
+// Nullish Coslescing Operator (??): null undefined
