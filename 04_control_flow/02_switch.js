53521e44a3eeea5293d334ecd288afc2cfb175a0
Akash Chauhan <121815603+akashchauhan555@users.noreply.github.com>
2024-05-01 11:17:15 +0000

    completed

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,50 @@
+// switch (key) {
+//   case value:
+
+//     break;
+
+//   default:
+//     break;
+// }
+
+const month = 5;
+
+// switch (month) {
+//   case 1:
+//     console.log("January");
+//     break;
+//   case 2:
+//     console.log("February");
+//     break;
+//   case 3:
+//     console.log("March");
+//     break;
+//   case 4:
+//     console.log("April");
+//     break;
+//   case 5:
+//     console.log("May");
+//     break;
+//   default:
+//     console.log("Wrong choice");
+//     break;
+// }
+
+const names = "anish";
+
+switch (names) {
+  case "akash":
+    console.log("akash");
+    break;
+  case "alakh":
+    console.log("alakh");
+    break;
+  case "anuj":
+    console.log("anuj");
+    break;
+  case "anish":
+    console.log("anish");
+    break;
+    default:
+      console.log("Wrong choice");
+}
