2ceca5fc61bd03c55ec97a8f161dd4ad2105f00f
Akash Chauhan <121815603+akashchauhan555@users.noreply.github.com>
2024-05-07 11:33:03 +0000

    half done

--- 05_iterations/01_forloop.js
+++ 05_iterations/01_forloop.js
@@ -2,5 +2,37 @@
 
 for(let i = 9; i < 10; i++) {
     const element = i;
-    console.log(i);
+   //console.log(i);                  // => 9
+}
+
+for(let i = 1; i <= 10; i++) {
+    // console.log(`Outer loop value: ${i}`);
+    for(let j = 1; j <= 10; j++ ) {
+        // console.log(i + '*' + j + ' = ' + i*j);
+    }
+}
+
+let myArray = ['akash', 'alakh', 'nikhil', 'anuj']
+
+//console.log(myArray.length);
+
+for (let i = 0; i < myArray.length; i++) {
+    const element = myArray[i];
+    //console.log(element);
+}  
+
+for(let i = 1; i <= 20; i++) {
+    if(i === 5) {
+       // console.log(`Detected 5 `);
+        break
+    }
+   // console.log(`Value of i is ${i}`);
+}
+
+for(let i = 1; i <= 20; i++) {
+    if(i === 5) {
+        console.log(`Detected 5 `);
+        continue
+    }
+    console.log(`Value of i is ${i}`);
 }
