b6c17928e75e53c2524ed951d8be5df392e07c58
Akash Chauhan <121815603+akashchauhan555@users.noreply.github.com>
2024-07-30 10:37:56 +0000

    done

--- 05_iterations/02_whileloop.js
+++ 05_iterations/02_whileloop.js
@@ -1,18 +1,25 @@
 
-let index = 0
-while (index <= 10) {
-  //console.log(`Value of index is ${index}`);
-  index = index + 2;
-}
+// let index = 0
+// while (index <= 10) {
+//   //console.log(`Value of index is ${index}`);
+//   index = index + 2;
+// }
 
 
-let myArr = ["flash", "batman", "superman", "aquaman"]
+// let myArr = ["flash", "batman", "superman", "aquaman"]
 
 
-let arr = 0
-while(arr < myArr.length) {
-  console.log(`Value of array is ${myArr[arr]}`);
-  arr = arr + 1;
-}
+// let arr = 0
+// while(arr < myArr.length) {
+//   console.log(`Value of array is ${myArr[arr]}`);
+//   arr = arr + 1;
+// }
+
+let score = 1
+
+do {
+  console.log(`Score is ${score}`);
+  score++
+} while (score <= 10);
 
 
