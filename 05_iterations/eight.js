246eccc399674d7e8334c328f5f54718443821cf
Akash Chauhan <121815603+akashchauhan555@users.noreply.github.com>
2024-08-04 07:48:01 +0000

    final done

--- 05_iterations/eight.js
+++ 05_iterations/eight.js
@@ -1,4 +1,4 @@
-const myNums = [1, 2, 3, 4, 5, 6, 7]
+//const myNums = [1, 2, 3, 4, 5, 6, 7]
 
 // const initialValue = 0
 
@@ -10,12 +10,12 @@ const myNums = [1, 2, 3, 4, 5, 6, 7]
 
 
 
-const myTotal = myNums.reduce( function(acc,currval) {
-  console.log(`acc: ${acc} and currval: ${currval}`);
-  return acc + currval
-},0)
+//const myTotal = myNums.reduce( function(acc,currval) {
+  //console.log(`acc: ${acc} and currval: ${currval}`);
+  //return acc + currval
+  //},0)
 
-console.log(myTotal);
+// console.log(myTotal);
 
 /* Output: acc: 0 and currval: 1
 acc: 1 and currval: 2
@@ -25,4 +25,37 @@ acc: 10 and currval: 5
 acc: 15 and currval: 6
 acc: 21 and currval: 7
 28 */
+
+
+const myNums = [1, 2, 3, 4, 6, 7, 8]
+
+const newNums = myNums.reduce( (acc,currval) => acc + currval, 0)
+
+//console.log(newNums);
+
+
+const shoppingCart = [
+  {
+    itemName: "py course",
+    price: 999
+  },
+  {
+    itemName: "js course",
+    price: 499
+  },
+  {
+    itemName: "data science course",
+    price: 12999
+  },
+  {
+    itemName: "web dev course",
+    price: 1999
+  },
+]
+
+const priceToPay = shoppingCart.reduce( (acc, item) => acc + item.price, 0)
+
+console.log(priceToPay);
+
+
 
